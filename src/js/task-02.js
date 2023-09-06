894cf3981fd3ad2691822f496bc4a7c09da3c9b3
MarcinT82 <marcin.g.turek@gmail.com>
2023-09-06 14:28:19 +0200

    workingOnSixth

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,38 +7,15 @@ const ingredients = [
   "Condiments",
 ];
 const list = document.querySelector("#ingredients");
+const listArray = [];
 
-const listItemOne = document.createElement("li");
-listItemOne.classList.add("item");
-listItemOne.textContent = `${ingredients[0]}`;
+ingredients.forEach((ingredient) => {
+  const item = document.createElement("li");
+  item.className = "item";
+  item.textContent = ingredient;
+  listArray.push(item);
+});
 
-const listItemTwo = document.createElement("li");
-listItemTwo.classList.add("item");
-listItemTwo.textContent = `${ingredients[1]}`;
-
-const listItemThree = document.createElement("li");
-listItemThree.classList.add("item");
-listItemThree.textContent = `${ingredients[2]}`;
-
-const listItemFour = document.createElement("li");
-listItemFour.classList.add("item");
-listItemFour.textContent = `${ingredients[3]}`;
-
-const listItemFive = document.createElement("li");
-listItemFive.classList.add("item");
-listItemFive.textContent = `${ingredients[4]}`;
-
-const listItemSix = document.createElement("li");
-listItemSix.classList.add("item");
-listItemSix.textContent = `${ingredients[5]}`;
-
-list.append(
-  listItemOne,
-  listItemTwo,
-  listItemThree,
-  listItemFour,
-  listItemFive,
-  listItemSix
-);
+list.append(...listArray);
 
 console.log(list);
--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,8 +1,8 @@
 const textInput = document.querySelector("#validation-input");
 const dataLength = textInput.getAttribute("data-length");
-
 textInput.addEventListener("blur", (event) => {
   if (event.currentTarget.value.length == dataLength) {
+    textInput.classList.remove("invalid");
     textInput.classList.add("valid");
   } else {
     textInput.classList.add("invalid");
