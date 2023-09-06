aa29a8805fb43acc84c53b69169fd9336099dd06
MarcinT82 <marcin.g.turek@gmail.com>
2023-09-06 15:03:42 +0200

    parseInt

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,7 +1,7 @@
 const textInput = document.querySelector("#validation-input");
 const dataLength = textInput.getAttribute("data-length");
 textInput.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length == dataLength) {
+  if (event.currentTarget.value.length === parseInt(dataLength)) {
     textInput.classList.remove("invalid");
     textInput.classList.add("valid");
   } else {
